32968fa79cf0133498fddc6fb8b63c8331fdcfb5
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-12-30 18:22:26 +0800

    Add Home template

--- market.py
+++ market.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return "hi!"
+def home_page():
+    return render_template('home.html')
+
+# @app.route('/about/<username>')
+# def about_page(username):
+#     return f'<h1>This is the About Page {username}</h1>'
