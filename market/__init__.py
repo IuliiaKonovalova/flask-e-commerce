58d7ce6fd9336a7a5d737b697f170ca2de8aa716
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-12-31 16:07:26 +0800

    Add register templates

--- market/__init__.py
+++ market/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] ='ca03e25403cee8ea2161554e'
 db = SQLAlchemy(app)
 
 from market import routes
