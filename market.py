1a7bb8a183ae13fa6e7234c7a3700dd87c7e7dd5
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-12-31 12:42:17 +0800

    Connect app to SQLAlchemy

--- market.py
+++ market.py
@@ -2,21 +2,35 @@ import os
 from flask import (
     Flask, flash, render_template, redirect, request, session, url_for
 )
-from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
-from werkzeug.security import generate_password_hash, check_password_hash
-if os.path.exists("env.py"):
-    import env
+from flask_sqlalchemy import SQLAlchemy
+# from flask_pymongo import PyMongo
+# from bson.objectid import ObjectId
+# from werkzeug.security import generate_password_hash, check_password_hash
+# if os.path.exists("env.py"):
+#     import env
 
 
 
 app = Flask(__name__)
 
-app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
-app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
-app.secret_key = os.environ.get("SECRET_KEY")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+db = SQLAlchemy(app)
 
-mongo = PyMongo(app)
+class Item(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(length=30), nullable=False, unique=True)
+    price = db.Column(db.Integer(), nullable=False)
+    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
+    description = db.Column(db.String(length=1024), nullable=False, unique=True)
+
+    def __repr__(self):
+        return f'Item {self.name}'
+
+# app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+# app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+# app.secret_key = os.environ.get("SECRET_KEY")
+
+# mongo = PyMongo(app)
 
 
 @app.route('/')
@@ -27,7 +41,7 @@ def home_page():
 
 @app.route('/market')
 def market_page():
-    items = list(mongo.db.products.find())
+    items = Item.query.all()
     return render_template('market.html', items=items)
 
 
