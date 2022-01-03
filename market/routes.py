9b5bcc131daa278dea1e312077fd72f15a51a448
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-01-03 15:51:45 +0800

    Add buy method to Item class

--- market/routes.py
+++ market/routes.py
@@ -19,12 +19,13 @@ def market_page():
         purchase_item = request.form.get('purchased_item')
         p_item_object = Item.query.filter_by(name=purchase_item).first()
         if p_item_object:
-            p_item_object.owner = current_user.id
-            current_user.budget =+ p_item_object.price
-            db.session.commit()
-            flash(f'Congratulations! You purchased {p_item_object} for {p_item_object.price}')
+            if current_user.can_purchase(p_item_object):
+                p_item_object.buy(current_user)
+                flash(f'Congratulations! You purchased {p_item_object} for {p_item_object.price}$', category='success')
+            else:
+                flash(f'Unfortunately, you don\'t have enough money to purchase {p_item_object.name}', category='danger')
+        return redirect(url_for('market_page'))
     if request.method == "GET":
-
         items = Item.query.filter_by(owner=None)
         return render_template('market.html', items=items, purchase_form=purchase_form)
 
