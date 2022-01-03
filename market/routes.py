407df14e3f2d9234e8caf50891fd5247e0747807
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-01-03 17:54:02 +0800

    Add selling functionality

--- market/routes.py
+++ market/routes.py
@@ -24,6 +24,14 @@ def market_page():
                 flash(f'Congratulations! You purchased {p_item_object} for {p_item_object.price}$', category='success')
             else:
                 flash(f'Unfortunately, you don\'t have enough money to purchase {p_item_object.name}', category='danger')
+        sold_item = request.form.get('sold_item')
+        s_item_object = Item.query.filter_by(name=sold_item).first()
+        if s_item_object:
+            if current_user.can_sell(s_item_object):
+                s_item_object.sell(current_user)
+                flash(f"Congratulations! You sold {s_item_object.name} back to market!", category='success')
+            else:
+                flash(f"Something went wrong with selling {s_item_object.name}", category='danger')
         return redirect(url_for('market_page'))
     if request.method == "GET":
         items = Item.query.filter_by(owner=None)
