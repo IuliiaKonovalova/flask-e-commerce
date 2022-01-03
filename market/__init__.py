6b00c74d2f385cd3f96d52ac1adfdcb4840dfd46
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-01-03 13:39:57 +0800

    Add smart login message

--- market/__init__.py
+++ market/__init__.py
@@ -9,4 +9,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = 'ec9439cfc6c796ae2029594d'
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
+login_manager,login_view = 'login_page'
+login_manager.login_messager_category = 'info'
 from market import routes
